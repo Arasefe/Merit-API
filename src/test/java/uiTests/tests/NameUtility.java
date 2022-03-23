838ed8095edf28b41078f585488e77d0c39bb324
Arasefe <ismail_yildirim69@yahoo.com>
2022-03-23 15:03:21 -0500

    Code refactored

--- src/test/java/uiTests/tests/NameUtility.java
+++ src/test/java/uiTests/tests/NameUtility.java
@@ -2,38 +2,40 @@ package uiTests.tests;
 
 
 public class NameUtility {
+    static String fullName;
     public static void main(String[] args) {
-        System.out.println(convertNameToInitials("Arasm"));
+
+        System.out.println(convertNameToInitials("Aras     Efe Yildirim   "));
 
     }
 
 
     public static String convertNameToInitials(String fullName) {
+        fullName = fullName.trim().replaceAll("\\s+", " ");
         if(fullName.length()==0){
             throw new RuntimeException("Please give a valid full name");
         }else if (!fullName.contains(" ")) {
-            char firstInitial = fullName.charAt(0);
-            return firstInitial + ".";
+            String firstInitial =""+fullName.charAt(0);
+            return firstInitial.toUpperCase()+".";
+
         } else if (fullName.indexOf(" ") == fullName.lastIndexOf(" ")) {
             int firstSpace = fullName.indexOf(" ");
             String firstName = fullName.substring(0, firstSpace);
             String lastName = fullName.substring(firstSpace + 1);
-            char firstInitial = firstName.charAt(0);
-            char lastInitial = lastName.charAt(0);
-            return firstInitial + "." + lastInitial + ".";
+            String firstInitial =""+firstName.charAt(0);
+            String lastInitial = ""+lastName.charAt(0);
+            return firstInitial.toUpperCase()+ "." + lastInitial .toUpperCase()+ ".";
         } else {
             int firstSpace = fullName.indexOf(" ");
             int lastSpace = fullName.lastIndexOf(" ");
             String firstName = fullName.substring(0, firstSpace);
-            char firstInitial = firstName.charAt(0);
-
+            String firstInitial = ""+firstName.charAt(0);
             String middleName = fullName.substring(firstSpace + 1, lastSpace);
-            char middleInitial = middleName.charAt(0);
-
+            String middleInitial = ""+middleName.charAt(0);
             String lastName = fullName.substring(lastSpace + 1);
-            char lastInitial = lastName.charAt(0);
+            String lastInitial = ""+lastName.charAt(0);
 
-            return "" + firstInitial + "." + middleInitial + "." + lastInitial + ".";
+            return firstInitial.toUpperCase() + "." + middleInitial.toUpperCase() + "." + lastInitial.toUpperCase() + ".";
 
         }
     }
